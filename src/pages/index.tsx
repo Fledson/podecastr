0c1846a97831d9310b916b743bb62b7366c30e94
Fledson Henrique <fledsonhenrique@gmail.com>
2021-04-21 16:26:52 -0300

    colocando boas praticas a home, tratando as informações e ajusto o consumo da api com axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,78 @@
+import { GetStaticProps } from 'next'; //tipagem da função
+import { api } from '../services/api';
 
-export default function Home(props) {
+import {format, parseISO} from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+
+
+/** ====================== TIPAGENS ===================== */
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+/** ====================== RENDERIZANDO A TELA ===================== */
+export default function Home(props:HomeProps) {
    
+  
   return (
     <div>
-      {/* <h1>INDEX</h1>
-      <p>{JSON.stringify(props.episodes)}</p> */}
+      <h1>INDEX</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
     </div>
   )
 }
 
-export async function getStaticProps(){
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json() 
+/** ====================== CONSUMINDO API ===================== */
 
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort:"published_at",
+      _order: "desc"
+    }
+  })
+
+  /**> ====================== TRATANDO AS INFORMAÇÕES ===================== */
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAs: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
+
+  /** ====================== TRANTANDO DADOS -> OBTENDO LISTA DE EPISODIOS ===================== */
+  /** Separando os dados e strings diferentes */
+
+  const latestEpisodes = episodes.slice(0, 2); //-> pegando os 2 primeiros da consulta
+  const allEpisodes = episodes.slice(2, episodes.length) //-> pegando o resto dos episodes
+
+  /** ====================== RETORNO A HOME ===================== */
   return {
     props: { 
-      episodes: data,
+      latestEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8,
   }
---
+++ src/utils/convertDurationToTimeString.ts
@@ -0,0 +1,14 @@
+// convertendo duração de segundos para hora minuto e segundos
+
+export function convertDurationToTimeString(duration){
+  const hours = Math.floor(duration / 3600);// => pegando o menor numero da divisão 
+  const minutes = Math.floor((duration % 60) / 60); // => resto da divisão do calculo acima e dividindo por 60
+  const seconds = Math.floor(duration % 60);
+
+  const timeString = [hours, minutes, seconds]
+    .map(unit => String(unit).padStart(2, "0"))
+    .join(":");
+  
+  return timeString;
+   
+}
